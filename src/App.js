7fdae664072a8adb6c292b07e22a08a25ee67876
Mehedi <hassanmehedi26@gmail.com>
2025-03-02 14:47:16 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css'; // import a css file
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello, React!</h1>
+      <h1>This is App.js component</h1>
+      <button>Click me</button>
+    </div>
+  );
+}
+
+export default App;
